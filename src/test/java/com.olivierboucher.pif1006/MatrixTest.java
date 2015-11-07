8193ffa2e45261408269699aae8485656f9f30f8
Olivier Boucher <olivier@bouchernolet.com>
2015-11-07 09:17:16 -0500

    Added test

---
+++ src/test/java/com.olivierboucher.pif1006/MatrixTest.java
@@ -0,0 +1,123 @@
+package com.olivierboucher.pif1006;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by olivier on 2015-11-07.
+ */
+public class MatrixTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNumRows() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNumColumns() throws Exception {
+
+    }
+
+    @Test
+    public void testGetMatrix() throws Exception {
+
+    }
+
+    @Test
+    public void testGetElement() throws Exception {
+
+    }
+
+    @Test
+    public void testSetElement() throws Exception {
+
+    }
+
+    @Test
+    public void testGetLine() throws Exception {
+
+    }
+
+    @Test
+    public void testGetColumn() throws Exception {
+
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+
+    }
+
+    @Test
+    public void testScalarProduct() throws Exception {
+
+    }
+
+    @Test
+    public void testMatrixMultiplication() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTrace() throws Exception {
+
+    }
+
+    @Test
+    public void testGetDeterminant() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSubmatrix() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTransposed() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCoMatrix() throws Exception {
+
+    }
+
+    @Test
+    public void testGetInversed() throws Exception {
+
+    }
+
+    @Test
+    public void testIsSquared() throws Exception {
+
+    }
+
+    @Test
+    public void testIsTriangular() throws Exception {
+
+    }
+
+    @Test
+    public void testIsRegular() throws Exception {
+
+    }
+
+    @Test
+    public void testToString() throws Exception {
+
+    }
+}

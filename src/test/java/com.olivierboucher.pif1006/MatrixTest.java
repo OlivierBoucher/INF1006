128d2470a78b566a38d12218ed9b0be9234aa408
yann510 <y.thibodeau1@gmail.com>
2015-11-21 10:59:16 -0500

    Added unit test

--- src/test/java/com.olivierboucher.pif1006/MatrixTest.java
+++ src/test/java/com.olivierboucher.pif1006/MatrixTest.java
@@ -57,23 +57,40 @@ public class MatrixTest {
     }
 
     @Test
-    public void testAdd() throws Exception {
-
+    public void testAddShouldPass() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {1, 2, 3}, {3, 2, 1}, {2, 1, 3}
+        });
+        Matrix b = new Matrix(new double[][]{
+                {1, 2, 3}, {3, 2, 1}, {2, 1, 3}
+        });
+        Matrix c = a.add(b);
+        Matrix r = new Matrix(new double[][]{
+                {2, 4, 6}, {6, 4, 2}, {4, 2, 6}
+        });
+        assertEquals(r.getMatrix(), c.getMatrix());
     }
 
     @Test
     public void testScalarProduct() throws Exception {
-
+        Matrix a = new Matrix(new double[][]{
+                {1, 2, 3}, {3, 2, 1}, {2, 1, 3}
+        });
+        Matrix b = a.scalarProduct(3);
+        Matrix r = new Matrix(new double[][]{
+                {3, 6, 9}, {9, 6, 3}, {6, 3, 9}
+        });
+        assertEquals(r.getMatrix(), b.getMatrix());
     }
 
     @Test
     public void testMatrixMultiplication() throws Exception {
         Matrix a = new Matrix(new double[][]{
-                {1,2,3},{3,2,1},{2,1,3}
+                {1, 2, 3}, {3, 2, 1}, {2, 1, 3}
         });
 
         Matrix b = new Matrix(new double[][]{
-                {4,5,6},{6,5,4},{4,6,5}
+                {4, 5, 6}, {6, 5, 4}, {4, 6, 5}
         });
 
         Matrix c = a.matrixMultiplication(b);
@@ -81,26 +98,48 @@ public class MatrixTest {
                 {28, 33, 29}, {28, 31, 31}, {26, 33, 31}
         });
 
-        for(int i = 0; i< c.getNumRows(); i++){
-            for(int j = 0; j< c.getNumColumns(); j++){
-                assertEquals(r.getElement(i,j), c.getElement(i, j), 0);
+        for (int i = 0; i < c.getNumRows(); i++) {
+            for (int j = 0; j < c.getNumColumns(); j++) {
+                assertEquals(r.getElement(i, j), c.getElement(i, j), 0);
             }
         }
     }
 
     @Test
     public void testGetTrace() throws Exception {
-
+        Matrix a = new Matrix(new double[][]{
+                {1, 2, 3},
+                {3, 2, 1},
+                {2, 1, 3}
+        });
+        double expected = 6.0;
+        assertEquals(expected, a.getTrace(), 0);
     }
 
     @Test
     public void testGetDeterminant() throws Exception {
-
+        Matrix a = new Matrix(new double[][]{
+                {1, 2, 3},
+                {3, 2, 1},
+                {2, 1, 3}
+        });
+        double expected = -12;
+        assertEquals(expected, a.getDeterminant(), 0);
     }
 
     @Test
     public void testGetSubmatrix() throws Exception {
-
+        Matrix a = new Matrix(new double[][]{
+                {1, 2, 3},
+                {3, 2, 1},
+                {2, 1, 3}
+        });
+        Matrix b = a.getSubmatrix(1, 1);
+        Matrix c = new Matrix(new double[][]{
+                {1, 3},
+                {2, 3}
+        });
+        assertEquals(c.getMatrix(), b.getMatrix());
     }
 
     @Test
@@ -118,9 +157,9 @@ public class MatrixTest {
                 {3, 1, 3}
         });
 
-        for(int i = 0; i< c.getNumRows(); i++){
-            for(int j = 0; j< c.getNumColumns(); j++){
-                assertEquals(r.getElement(i,j), c.getElement(i, j), 0);
+        for (int i = 0; i < c.getNumRows(); i++) {
+            for (int j = 0; j < c.getNumColumns(); j++) {
+                assertEquals(r.getElement(i, j), c.getElement(i, j), 0);
             }
         }
     }
@@ -140,9 +179,9 @@ public class MatrixTest {
                 {-3, 6, -3}
         });
 
-        for(int i = 0; i< c.getNumRows(); i++){
-            for(int j = 0; j< c.getNumColumns(); j++){
-                assertEquals(r.getElement(i,j), c.getElement(i, j), 0);
+        for (int i = 0; i < c.getNumRows(); i++) {
+            for (int j = 0; j < c.getNumColumns(); j++) {
+                assertEquals(r.getElement(i, j), c.getElement(i, j), 0);
             }
         }
     }
@@ -157,31 +196,96 @@ public class MatrixTest {
 
         Matrix c = a.getInversed();
         Matrix r = new Matrix(new double[][]{
-                {1.0/3.0, 1.0/3.0, 0},
-                {-(2.0/3.0), 1.0/3.0, 1},
+                {1.0 / 3.0, 1.0 / 3.0, 0},
+                {-(2.0 / 3.0), 1.0 / 3.0, 1},
                 {1, -1, -1}
         });
 
-        for(int i = 0; i< c.getNumRows(); i++){
-            for(int j = 0; j< c.getNumColumns(); j++){
-                assertEquals(r.getElement(i,j), c.getElement(i, j), 0);
+        for (int i = 0; i < c.getNumRows(); i++) {
+            for (int j = 0; j < c.getNumColumns(); j++) {
+                assertEquals(r.getElement(i, j), c.getElement(i, j), 0);
             }
         }
     }
 
     @Test
-    public void testIsSquared() throws Exception {
-
+    public void testIsSquaredShouldPass() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {2, 1, 1},
+                {1, -1, -1},
+                {1, 2, 1}
+        });
+        Boolean expected = true;
+        assertEquals(expected, a.isSquared());
     }
 
     @Test
-    public void testIsTriangular() throws Exception {
-
+    public void testIsSquaredShouldFail() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {2, 1, 1},
+                {1, -1, -1},
+        });
+        Boolean expected = false;
+        assertEquals(expected, a.isSquared());
     }
 
     @Test
-    public void testIsRegular() throws Exception {
+    public void testIsTriangularAnyShouldFail() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {2, 1, 1},
+                {1, -1, -1},
+                {1, 2, 1}
+        });
+        Boolean expected = false;
+        assertEquals(expected, a.isTriangular(Matrix.TriangleType.ANY, false));
+    }
 
+    @Test
+    public void testIsTriangularAnyShouldSuccess() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {2, 0, 0},
+                {1, 1, 0},
+                {1, 2, 1}
+        });
+        Boolean expected = true;
+        assertEquals(expected, a.isTriangular(Matrix.TriangleType.ANY, false));
+    }
+
+    @Test
+    public void testIsTriangularStrictAnyShouldSuccess() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {0, 0, 0},
+                {1, 0, 0},
+                {1, 2, 0}
+        });
+
+        Boolean expected = true;
+        assertEquals(expected, a.isTriangular(Matrix.TriangleType.ANY, true));
+
+        Matrix b = new Matrix(new double[][]{
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}
+        });
+        assertEquals(expected, b.isTriangular(Matrix.TriangleType.INFERIOR, true));
+    }
+
+    @Test
+    public void testIsRegularShouldSuccess() throws Exception {
+        Matrix a = new Matrix(new double[][]{
+                {1, 2, 3},
+                {3, 2, 1},
+                {2, 1, 3}
+        });
+        Boolean expected = true;
+        assertEquals(expected, a.isRegular());
+    }
+
+    @Test
+    public void testIsRegularShouldFail() throws Exception {
+        Matrix a = new Matrix(new double[][]{{1, 0, 3}, {3, 0, 1}, {2, 0, 3}});
+        Boolean expected = false;
+        assertEquals(expected, a.isRegular());
     }
 
     @Test
